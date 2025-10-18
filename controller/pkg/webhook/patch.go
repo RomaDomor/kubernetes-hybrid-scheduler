a07a37c7bd6b3b4fee7f3634c7dd5fe0c2fb1653
RomaDomor <roman@domoratskiy.com>
2025-10-18 14:36:07 +0300

    Extract patch to separate file

---
+++ controller/pkg/webhook/patch.go
@@ -0,0 +1,111 @@
+package webhook
+
+import (
+	"encoding/json"
+	"kubernetes-hybrid-scheduler/controller/pkg/decision"
+	"strings"
+	"time"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func (s *Server) buildPatchResponse(
+	pod *corev1.Pod,
+	res decision.Result,
+) *admissionv1.AdmissionResponse {
+	patches := []map[string]interface{}{}
+
+	// Ensure annotations exist
+	if pod.Annotations == nil {
+		patches = append(patches, map[string]interface{}{
+			"op":    "add",
+			"path":  "/metadata/annotations",
+			"value": map[string]string{},
+		})
+	}
+
+	// Add decision annotations
+	patches = append(patches,
+		addAnnotation("scheduling.hybrid.io/decision",
+			string(res.Location)),
+		addAnnotation("scheduling.hybrid.io/timestamp",
+			time.Now().Format(time.RFC3339)),
+		addAnnotation("scheduling.hybrid.io/reason", res.Reason),
+	)
+
+	// Ensure nodeSelector exists
+	if pod.Spec.NodeSelector == nil {
+		patches = append(patches, map[string]interface{}{
+			"op":    "add",
+			"path":  "/spec/nodeSelector",
+			"value": map[string]string{},
+		})
+	}
+
+	// Add location-specific patches
+	if res.Location == decision.Edge {
+		patches = append(patches, map[string]interface{}{
+			"op":    "add",
+			"path":  "/spec/nodeSelector/node.role~1edge",
+			"value": "true",
+		})
+	} else {
+		patches = append(patches,
+			map[string]interface{}{
+				"op":    "add",
+				"path":  "/spec/nodeSelector/node.role~1cloud",
+				"value": "true",
+			},
+			addCloudToleration(len(pod.Spec.Tolerations) == 0),
+		)
+	}
+
+	patchBytes, _ := json.Marshal(patches)
+	pt := admissionv1.PatchTypeJSONPatch
+
+	return &admissionv1.AdmissionResponse{
+		Allowed:   true,
+		Patch:     patchBytes,
+		PatchType: &pt,
+	}
+}
+
+func addAnnotation(key, val string) map[string]interface{} {
+	return map[string]interface{}{
+		"op":    "add",
+		"path":  "/metadata/annotations/" + escapeJSONPointer(key),
+		"value": val,
+	}
+}
+
+func addCloudToleration(isFirst bool) map[string]interface{} {
+	tol := map[string]string{
+		"key":      "virtual-node.liqo.io/not-allowed",
+		"operator": "Equal",
+		"value":    "true",
+		"effect":   "NoSchedule",
+	}
+
+	path := "/spec/tolerations"
+	if !isFirst {
+		path += "/-"
+	}
+
+	return map[string]interface{}{
+		"op":   "add",
+		"path": path,
+		"value": func() interface{} {
+			if isFirst {
+				return []map[string]string{tol}
+			}
+			return tol
+		}(),
+	}
+}
+
+func escapeJSONPointer(s string) string {
+	s = strings.ReplaceAll(s, "~", "~0")
+	s = strings.ReplaceAll(s, "/", "~1")
+	return s
+}
